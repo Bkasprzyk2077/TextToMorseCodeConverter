3e479739f5d250e58af70cd023fb67ad921018ea
Bartłomiej Kasprzyk <dolarbartek@wp.pl>
2024-05-11 20:36:13 +0200

    file read

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+data = {}
+with open("morse.csv", "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        item = line.split(",")
+        data[item[0]] = item[1].strip()
+
