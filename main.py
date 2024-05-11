f362e20c1b9fda3232cffc358ee451e6cb496dc1
Bartłomiej Kasprzyk <dolarbartek@wp.pl>
2024-05-11 20:50:03 +0200

    convert characters

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ with open("morse.csv", "r") as file:
         item = line.split(",")
         data[item[0]] = item[1].strip()
 
+should_continue = True
+while should_continue:
+    user_choice = input("Enter data co convert: ")
+    converted_data = ""
+    for character in user_choice:
+        if character.upper() in data.keys():
+            converted_data += data[character.upper()]
+            converted_data += " "
+        elif character == " ":
+            converted_data += "\n"
+    print(converted_data)
